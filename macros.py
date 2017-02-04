766ca4ba9a5c614d97df7e60cb986a3c51e4bac4
Liam Schumm <liamschumm@icloud.com>
2017-02-04 14:33:00 -0600

    Add 'up' command

---
+++ macros.py
@@ -0,0 +1,16 @@
+"""
+[up.py]
+
+Cool aliases.
+"""
+
+import os
+
+def up(env, args, kwargs):
+    """[N]@Move up a directory N times."""
+    for i in range(args[0]):
+        os.chdir("..")
+        env.directory = os.getcwd()
+    
+verbs = {"up":up,
+        }
